7c166d46156990b3ba1dac2718cfe3af58780872
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-14 16:42:01 +0800

    Time: 446 ms (28.60%), Space: 94.8 MB (99.76%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -4,7 +4,9 @@ private:
     unordered_map<int,int> table;
 public:
     RandomizedSet() {
-        
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     bool insert(int val) {
