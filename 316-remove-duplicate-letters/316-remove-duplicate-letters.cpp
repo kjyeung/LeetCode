4c863cad22f9423cd4c44319963d41e03f10f225
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-15 14:47:30 +0800

    Time: 3 ms (82.63%), Space: 6.9 MB (29.33%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> all(256, 0);
-        vector<bool> in(256, false);
+        vector<int> count(256);
+        vector<bool> in(256);
         for(char c : s){
-            ++all[c];
+            ++count[c];
         }
+        
         string res;
         for(char c : s){
-            --all[c];
+            --count[c];
             if(in[c]) continue;
-            
-            while(!res.empty() && res.back() >= c && all[res.back()] > 0){
+            while(!res.empty() && res.back() >= c && count[res.back()] > 0){
                 in[res.back()] = false;
                 res.pop_back();
             }
             res.push_back(c);
             in[c] = true;
         }
-
         return res;
-        
-        
     }
 };
