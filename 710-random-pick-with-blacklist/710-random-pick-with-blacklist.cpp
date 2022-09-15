3a799c8a19074effd54eef7c67b1bd0df204602c
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-15 12:34:48 +0800

    Time: 273 ms (36.25%), Space: 70.8 MB (53.72%) - LeetHub

--- 710-random-pick-with-blacklist/710-random-pick-with-blacklist.cpp
+++ 710-random-pick-with-blacklist/710-random-pick-with-blacklist.cpp
@@ -2,6 +2,8 @@ class Solution {
 private:
     unordered_map<int,int> table;
     int size;
+    std::mt19937 gen;
+    std::uniform_int_distribution<> dis;
     
 public:
     Solution(int n, vector<int>& blacklist) {
@@ -12,16 +14,21 @@ public:
             table[b] = -777;
         }
         
+        
         for(int & b : blacklist){
             if(b >= size) continue;
             while(table.count(last)) --last;
             table[b] = last--;
         }
         
+        std::random_device rd; 
+        gen = std::mt19937(rd()); 
+        dis = std::uniform_int_distribution<>(0, size - 1);
+        
     }
     
     int pick() {
-        int num = rand() % size;
+        int num = dis(gen);
         if(table.count(num)) return table[num];
         return num;
     }
