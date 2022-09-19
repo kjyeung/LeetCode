d795e7a3527ef270f237f6836b38fdf5b4e6daa0
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-19 11:16:13 +0800

    Time: 6 ms (36.92%), Space: 7.2 MB (69.68%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int calc(vector<int>& nums, int x){
+    int calc(vector<int>& nums, int m){
         int ret = 1;
         int sum = 0;
         for(int & num : nums){
-            if(sum + num > x){
-                sum = 0;
+            if(sum + num > m){
                 ret += 1;
+                sum = 0;
             }
-            sum += num;
+            sum+=num;
         }
         return ret;
     }
     int splitArray(vector<int>& nums, int m) {
-        int left = *max_element(nums.begin(), nums.end()), right = accumulate(nums.begin(), nums.end(), 0);
+        int left = *max_element(nums.begin(), nums.end()), right = accumulate(nums.begin(), nums.end(), 1);
         while(left < right){
             int mid = left + (right - left) / 2;
             int t = calc(nums, mid);
-            if(t >m){
+            if(t > m){
                 left = mid + 1;
             }else{
                 right = mid;
