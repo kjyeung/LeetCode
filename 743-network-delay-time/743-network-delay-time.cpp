905eb3a705b43b9dd03c69565da1f32ac3193e82
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-20 10:37:12 +0800

    Time: 246 ms (34.79%), Space: 39.9 MB (74.29%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -7,32 +7,32 @@ public:
     };
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>> graph(n);
-        for(vector<int>& e : times){
-            int from = e[0] - 1;
-            int to = e[1] - 1;
-            int time = e[2];
-            graph[from].push_back(make_pair(to, time));
+        
+        for(vector<int>& t : times){
+            graph[t[0] - 1].push_back(make_pair(t[1]-1, t[2]));
         }
+        
         vector<int> ts(n, INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>, comp> pq;
         ts[k - 1] = 0;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, comp> pq;
         pq.push(make_pair(k - 1, 0));
         while(!pq.empty()){
-            pair<int,int> cur =pq.top();
+            pair<int,int> cur = pq.top();
             pq.pop();
             int to = cur.first;
             int time = cur.second;
             if(time > ts[to]) continue;
-            for(pair<int,int> t:graph[to]){
-                int next = t.first;
-                int nt = ts[to] + t.second;
-                if(nt < ts[next]){
-                    ts[next] = nt;
-                    pq.push(make_pair(next, nt));
+            for(pair<int,int>& p : graph[to]){
+                int next = p.first;
+                int newtime = p.second + ts[to];
+                if(newtime < ts[next]){
+                    ts[next] = newtime;
+                    pq.push(make_pair(next, newtime));
                 }
             }
         }
-        int res = *max_element(ts.begin(), ts.end());
+        int res = *max_element(ts.begin(),ts.end());
         return res == INT_MAX? -1:res;
+        
     }
 };
