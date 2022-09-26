bf26d7267893104184b9db7311242dcc7f2ba057
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-26 19:32:34 +0800

    Time: 236 ms (42.83%), Space: 40.1 MB (65.26%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    struct comp{
-        bool operator()(pair<int,int>& a, pair<int,int>& b){
-            return a.second > b.second;
-        }
-    };
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>> graph(n);
-        
-        for(vector<int>& t : times){
-            graph[t[0] - 1].push_back(make_pair(t[1]-1, t[2]));
+        for(vector<int>& t: times){
+            graph[t[0] - 1].push_back(make_pair(t[1] - 1, t[2]));
         }
         
-        vector<int> ts(n, INT_MAX);
-        ts[k - 1] = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, comp> pq;
+        vector<int> dis(n, INT_MAX);
+        dis[k - 1] = 0;
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         pq.push(make_pair(k - 1, 0));
+        
         while(!pq.empty()){
-            pair<int,int> cur = pq.top();
+            pair<int,int> cur= pq.top();
             pq.pop();
             int to = cur.first;
-            int time = cur.second;
-            if(time > ts[to]) continue;
-            for(pair<int,int>& p : graph[to]){
+            int cost = cur.second;
+            if(cost > dis[to]) continue;
+            for(pair<int,int> p : graph[to]){
                 int next = p.first;
-                int newtime = p.second + ts[to];
-                if(newtime < ts[next]){
-                    ts[next] = newtime;
-                    pq.push(make_pair(next, newtime));
+                int cnext = p.second + cost;
+                if(cnext < dis[next]){
+                    dis[next] = cnext;
+                    pq.push(make_pair(next, cnext));
                 }
             }
         }
-        int res = *max_element(ts.begin(),ts.end());
-        return res == INT_MAX? -1:res;
-        
+        int res = *max_element(dis.begin(), dis.end());
+        return res == INT_MAX? -1: res;
     }
 };
