15afa8bc9f49d904bcb148921aa2963cb027693d
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-26 16:59:21 +0800

    Time: 656 ms (50.43%), Space: 165 MB (80.83%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,56 +1,59 @@
 class LRUCache {
-public:
     struct Node{
-        int key, value;
-        Node* left, *right;
-        Node(int _key, int _value): key(_key), value(_value), left(NULL), right(NULL){}  
-    }*L, *R;;
+        int k, v;
+        Node *prev, *next;
+        Node(int _k, int _v): k(_k), v(_v), prev(nullptr), next(nullptr) {};
+    };
+    Node *L, *R;
     int n;
     unordered_map<int, Node*> hash;
-    
-    void remove(Node* p){
-        p->right->left = p->left;
-        p->left->right = p->right;
+public:
+    void remove(Node* node){
+        node->next->prev = node->prev;
+        node->prev->next = node->next;
     }
     
-    void insert(Node* p){
-        p->right = L->right;
-        p->left = L;
-        L->right->left = p;
-        L->right = p;
-        
+    void insert(Node* node){
+        L->next->prev = node;
+        node->next = L->next;
+        L->next = node;
+        node->prev = L;
     }
+    
     LRUCache(int capacity) {
         n = capacity;
-        L = new Node(-1, -1), R = new Node(-1,-1);
-        L->right = R;
-        R->left = L;
+        L = new Node(-1, -1);
+        R = new Node(-1, -1);
+        L->next = R;
+        R->prev = L;
+        hash.clear();
     }
     
     int get(int key) {
-        if(hash.count(key) == 0) return -1;
-        auto p = hash[key];
-        remove(p);
-        insert(p);
-        return p->value;
+        if(hash.find(key) == hash.end()) return -1;
+        Node* node = hash[key];
+        remove(node);
+        insert(node);
+        return node->v;
     }
     
     void put(int key, int value) {
-        if(hash.count(key)){
-            auto p = hash[key];
-            p->value = value;
-            remove(p);
-            insert(p);
+        if(hash.find(key) != hash.end()){
+            Node* node = hash[key];
+            node->v = value;
+            remove(node);
+            insert(node);
         }else{
             if(hash.size() == n){
-                auto p = R->left;
-                remove(p);
-                hash.erase(p->key);
-                delete p ;
+                Node* cur = R->prev;
+                hash.erase(cur->k);
+                remove(cur);
+                delete cur;
             }
-            auto p = new Node(key, value);
-            hash[key] = p;
-            insert(p);
+            Node* node = new Node(key, value);
+            hash[key] = node;
+            insert(node);
         }
+        return;
     }
 };
