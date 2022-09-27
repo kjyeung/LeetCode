26c1edab429dc1fbd0038e40b71df4d11d7e0bd7
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-27 15:20:06 +0800

    Time: 7 ms (17.93%), Space: 7 MB (98.39%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -1,100 +1,105 @@
+class Tweet{
+public:
+    int id;
+    int ts;
+    Tweet* next;
+    Tweet(int _id, int _ts): id(_id), ts(_ts), next(nullptr){}
+};
+
+class User{
+public:
+    int id;
+    unordered_set<int> following;
+    Tweet* tweets;
+     User(int userid){
+         id = userid;
+         tweets = nullptr;
+         follow(userid);
+     }
+    
+    void follow(int userid){
+        if(following.count(userid)) return;
+        following.insert(userid);
+        return;
+    }
+    
+    void unfollow(int userid){
+        if(!following.count(userid) || userid == this->id) return;
+        following.erase(userid);
+        return;
+    }
+    
+    void tweet(int tweetid, int timestemp){
+        Tweet* cur = new Tweet(tweetid, timestemp);
+        cur->next = tweets;
+        tweets = cur;
+        return;
+    }
+    
+};
+
 class Twitter {
 public:
     int timestemp;
+    unordered_map<int, User*> alluser;
+    int cap = 10;
     
-    class Tweet{
-    public:
-        int t;
-        int id;
-        Tweet* next;
-        Tweet(int _t, int _id): t(_t), id(_id), next(nullptr){}
-    };
-    
-    class User{
-    public:
-        int id;
-        Tweet* tweets;
-        unordered_set<int> following;
-        User(int id){
-            this->id = id;
-            this->tweets = nullptr;
-            following.clear();
-            follow(id);
-            
-        }
-        
-        void tweet(int ts, int id){
-            Tweet* tweet = new Tweet(ts, id);
-            tweet->next = this->tweets;
-            this->tweets = tweet;
-            return;
-        }
-        
-        void follow(int followeeId){
-            if(following.count(followeeId)) return;
-            following.emplace(followeeId);
-        }
-        
-        void unfollow(int followeeId){
-            if(!following.count(followeeId) || followeeId == this->id) return;
-            following.erase(followeeId);
-        }
-    };
-    
-    unordered_map<int, User*> allusers;
-    int Cap;
     Twitter() {
         timestemp = 0;
-        allusers.clear();
-        Cap = 10;
+        alluser.clear();
     }
     
     void postTweet(int userId, int tweetId) {
-        if(!allusers.count(userId)){
-            allusers[userId] = new User(userId);
+        if(!alluser.count(userId)){
+            alluser[userId] = new User(userId);
         }
-        
-        allusers[userId]->tweet(timestemp++, tweetId);
+        alluser[userId]->tweet(tweetId, timestemp++);
+        return;
     }
     
     vector<int> getNewsFeed(int userId) {
-        vector<int> res;
-        if(!allusers.count(userId)) return res;
         struct comp{
             bool operator()(Tweet* a, Tweet* b){
-                return a->t < b->t;
+                return a->ts < b->ts;
             }
         };
+        vector<int> res;
         priority_queue<Tweet*, vector<Tweet*>, comp> pq;
-        for(int id : allusers[userId]->following){
-            if(allusers[id]->tweets) pq.push(allusers[id]->tweets);
+        if(!alluser.count(userId)) return res;
+        
+        for(int uid : alluser[userId]->following){
+            User* cur = alluser[uid];
+            if(cur->tweets)  pq.push(cur->tweets);
         }
         
-        while(!pq.empty() && res.size() < Cap){
+        while(res.size() < cap && !pq.empty()){
             Tweet* cur = pq.top();
             pq.pop();
             res.push_back(cur->id);
-            if(cur->next) pq.push(cur->next);
+            if(cur->next){
+                pq.push(cur->next);
+            }
         }
-            
+        
         return res;
+        
     }
     
     void follow(int followerId, int followeeId) {
-        if(!allusers.count(followerId)){
-            allusers[followerId] = new User(followerId);
+        if(!alluser.count(followerId)){
+            alluser[followerId] = new User(followerId);
         }
-        
-        if(!allusers.count(followeeId)){
-            allusers[followeeId] = new User(followeeId);
+        if(!alluser.count(followeeId)){
+            alluser[followeeId] = new User(followeeId);
         }
-        
-        allusers[followerId]->follow(followeeId);
+        alluser[followerId]->follow(followeeId);
+        return;
     }
     
     void unfollow(int followerId, int followeeId) {
-        if(!allusers.count(followerId) || !allusers.count(followeeId)) return;
-        allusers[followerId]->unfollow(followeeId);
+        if(!alluser.count(followerId) || !alluser.count(followeeId)) return;
+        alluser[followerId]->unfollow(followeeId);
+        return;
     }
 };
 
