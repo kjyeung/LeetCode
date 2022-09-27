12715bf36fdfd7e67e9543351690ca06b8beba59
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-27 15:54:31 +0800

    Time: 315 ms (64.58%), Space: 19.9 MB (85.89%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    struct comp{
-        bool operator()(tuple<int,int,int>&a, tuple<int,int,int>&b){
-            return get<2>(a) > get<2>(b);
-        }
-    };
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights.front().size();
-        vector<vector<int>> distances(m, vector<int>(n, INT_MAX));
-        distances[0][0] = 0;
-        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, comp> pq;
+        vector<vector<int>> costs(m, vector<int>(n, INT_MAX));
+        costs[0][0] = 0;
+        
+        // -c, i, j
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> pq;
+        vector<vector<int>> ds{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
         pq.push(make_tuple(0, 0, 0));
-        vector<vector<int>> ds{{0, 1}, {1, 0}, {0, -1},{-1, 0}};
+        
         while(!pq.empty()){
             tuple<int,int,int> cur = pq.top();
             pq.pop();
-            int i = get<0>(cur);
-            int j = get<1>(cur);
-            int dis = get<2>(cur);
-            if(i == m - 1 && j == n - 1) return dis;
-            if(dis > distances[i][j]) continue;
+            int c = get<0>(cur);
+            int i = get<1>(cur);
+            int j = get<2>(cur);
+            if(c > costs[i][j]) continue;
+            if(i == m - 1 && j == n - 1) return c;
             for(vector<int>& d : ds){
                 int newi = i + d[0];
                 int newj = j + d[1];
-                if(newi <  0 || newi > m - 1 || newj < 0 || newj > n - 1) continue;
-                int newdis = max(distances[i][j], abs(heights[i][j]- heights[newi][newj]));
-                if(newdis < distances[newi][newj]){
-                    distances[newi][newj] = newdis;
-                    pq.push(make_tuple(newi, newj, newdis));
+                if(newi < 0 || newi > m - 1 || newj <0 || newj > n - 1) continue;
+                int newc = max(c, abs(heights[i][j] - heights[newi][newj]));
+                if(newc < costs[newi][newj]){
+                    costs[newi][newj] = newc;
+                    pq.push(make_tuple(newc, newi, newj));
                 }
             }
         }
-        return 0;
+        return costs[m - 1][n - 1];
     }
 };
