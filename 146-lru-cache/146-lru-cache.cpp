18feae3d94bed298a675f41a80c393bf3ce3b6a0
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-27 16:45:17 +0800

    Time: 504 ms (81.99%), Space: 164.3 MB (95.24%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,59 +1,70 @@
 class LRUCache {
-    struct Node{
-        int k, v;
-        Node *prev, *next;
-        Node(int _k, int _v): k(_k), v(_v), prev(nullptr), next(nullptr) {};
-    };
-    Node *L, *R;
-    int n;
-    unordered_map<int, Node*> hash;
 public:
+    struct Node{
+        int key, val;
+        Node* prev, *next;
+        Node(int _k, int _v): key(_k), val(_v), prev(nullptr), next(nullptr){}
+    } *L, *R;
+    
+    int cap;
+    unordered_map<int, Node*> table;
+    
     void remove(Node* node){
         node->next->prev = node->prev;
         node->prev->next = node->next;
     }
     
     void insert(Node* node){
-        L->next->prev = node;
         node->next = L->next;
+        L->next->prev = node;
         L->next = node;
         node->prev = L;
     }
     
     LRUCache(int capacity) {
-        n = capacity;
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+        cap = capacity;
         L = new Node(-1, -1);
-        R = new Node(-1, -1);
+        R = new Node(-1,-1);
         L->next = R;
         R->prev = L;
-        hash.clear();
+        table.clear();
     }
     
+    
     int get(int key) {
-        if(hash.find(key) == hash.end()) return -1;
-        Node* node = hash[key];
+        if(!table.count(key)) return -1;
+        Node* node = table[key];
         remove(node);
         insert(node);
-        return node->v;
+        return node->val;
     }
     
     void put(int key, int value) {
-        if(hash.find(key) != hash.end()){
-            Node* node = hash[key];
-            node->v = value;
-            remove(node);
-            insert(node);
-        }else{
-            if(hash.size() == n){
-                Node* cur = R->prev;
-                hash.erase(cur->k);
-                remove(cur);
-                delete cur;
+        if(!table.count(key)){
+            if(table.size() == cap){
+                Node* lru = R->prev;
+                table.erase(lru->key);
+                remove(lru);
+                delete lru;
             }
-            Node* node = new Node(key, value);
-            hash[key] = node;
+            Node* cur = new Node(key, value);
+            table[key] = cur;
+            insert(cur);
+            
+        }else{
+            Node* node = table[key];
+            node->val = value;
+            remove(node);
             insert(node);
         }
         return;
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
