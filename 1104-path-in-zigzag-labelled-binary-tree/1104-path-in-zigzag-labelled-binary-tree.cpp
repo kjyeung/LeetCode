785d729a8fa329d91e401b31925cb119c6fb8f11
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-29 13:33:30 +0800

    Time: 3 ms (39.16%), Space: 6.4 MB (36.97%) - LeetHub

--- 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
         vector<int> res;
-        while(label >= 1){
+        while(label > 1){
             res.push_back(label);
             int level = log2(label);
-            int max = pow(2, level) - 1;
-            int min = pow(2, level - 1);
-            label = max - (label / 2 - min);
+            int upstair_low = pow(2, level - 1) ;
+            int upstair_high = pow(2, level) - 1;
+            label = upstair_high - (label / 2 - upstair_low);
         }
+        res.push_back(1);
         reverse(res.begin(), res.end());
         return res;
     }
