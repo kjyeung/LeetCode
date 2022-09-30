0703c15bde2b0904ea05f13d4a1cf1ab7e281f34
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-09-30 15:37:34 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (39.02%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int fib(int n) {
         if(n <= 1) return n;
-        int s1 = 0;
-        int s2 = 1;
-        int sum;
+        int s1 = 0, s2 = 1, sum;
         for(int i = 1; i<n;i++){
             sum = s1 + s2;
             s1 = s2;
