f088ca6f100bfaebf099afb818c8e3e35e5820da
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-04 20:27:49 +0800

    Time: 639 ms (34.24%), Space: 134.6 MB (81.55%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -4,11 +4,16 @@ public:
         deque<int> q;
         vector<int> res;
         for(int i = 0; i < k - 1; i++){
-            while(!q.empty() && nums[q.back()] < nums[i]) q.pop_back();
+            while(!q.empty() && nums[q.back()] < nums[i]){
+                q.pop_back();
+            }
             q.push_back(i);
         }
-        for(int i = k - 1; i < nums.size(); i++){
-            while(!q.empty() && nums[q.back()] < nums[i]) q.pop_back();
+        
+        for(int i = k - 1; i < nums.size();i++){
+            while(!q.empty() && nums[q.back()] < nums[i]){
+                q.pop_back();
+            }
             q.push_back(i);
             res.push_back(nums[q.front()]);
             if(q.front() == i - k + 1) q.pop_front();
