32338c51cbf75ab9fdbb9ddf83e1a0d8ce2eb3fd
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-05 16:37:06 +0800

    Time: 451 ms (95.06%), Space: 165.1 MB (79.85%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,10 +1,9 @@
 struct Node{
     int key, val;
-    Node* next, *prev;
-    Node(int _key, int _val): key(_key), val(_val){}
+    Node* prev, *next;
+    Node(int _key, int _val): key(_key), val(_val), prev(nullptr), next(nullptr){}
 };
 class LRUCache {
-private:
     Node* L, *R;
     int cap;
     unordered_map<int,Node*> table;
@@ -12,32 +11,33 @@ public:
     void remove(Node* node){
         node->next->prev = node->prev;
         node->prev->next = node->next;
+        return;
     }
     
     void insert(Node* node){
         L->next->prev = node;
         node->next = L->next;
-        node->prev = L;
         L->next = node;
+        node->prev = L;
+        return;
     }
     
     LRUCache(int capacity) {
-        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-        cap = capacity;
         L = new Node(-1, -1);
         R = new Node(-1, -1);
         L->next = R;
         R->prev = L;
+        cap = capacity;
         table.clear();
     }
+    
     
     int get(int key) {
         if(!table.count(key)) return -1;
         Node* cur = table[key];
-        int ret = cur->val;
         remove(cur);
         insert(cur);
-        return ret;
+        return cur->val;
     }
     
     void put(int key, int value) {
@@ -53,10 +53,12 @@ public:
                 remove(lru);
                 delete lru;
             }
-            Node* node = new Node(key, value);
-            insert(node);
-            table[key] = node;
+            
+            Node* cur = new Node(key, value);
+            table[key] = cur;
+            insert(cur);
         }
+        
         return;
     }
 };
