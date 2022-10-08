310ae77a02804c68ba8f1232ea26a9862603865a
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-08 16:38:03 +0800

    Time: 153 ms (54.18%), Space: 14.4 MB (77.34%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if(amount == 0) return 0;
+        int ret = 0;
         vector<int> table(amount + 1, amount + 1);
         table[0] = 0;
-        for(int i = 1; i <= amount; i++){
+        for(int i = 0; i < table.size(); i++){
             for(int c : coins){
                 if(i - c < 0) continue;
-                if(table[i - c] + 1 < table[i]){
-                    table[i] = table[i - c] + 1;
-                }
+                table[i] = min(table[i], table[i - c] + 1);
             }
         }
         return table[amount] == amount + 1? -1 : table[amount];
