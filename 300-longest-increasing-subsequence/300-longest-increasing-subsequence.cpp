69188d967eda92e9db1f0e2e5bd6d799d31c5ee1
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-09 10:12:37 +0800

    Time: 12 ms (90.34%), Space: 10.4 MB (70.60%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> table;
-        for(int num : nums){
-            if(table.empty()){
-                table.push_back(num);
+        vector<int> tmp;
+        for(int i = 0; i < nums.size(); i++){
+            if(tmp.empty()){
+                tmp.push_back(nums[i]);
                 continue;
-            } 
-            int left = 0, right = table.size();
+            }
+            
+            int left = 0, right = tmp.size();
             while(left < right){
                 int mid = left + (right - left) / 2;
-                if(table[mid] >= num){
-                    right = mid;
-                }else left = mid + 1;
+                if(tmp[mid] < nums[i]){
+                    left = mid + 1;
+                }else right = mid;
             }
-            if(left == table.size()) table.push_back(num);
-            else table[left] = num;
             
+            if(left == tmp.size()){
+                tmp.push_back(nums[i]);
+            }else{
+                tmp[right] = nums[i];
+            }
         }
-        return table.size();
+        return tmp.size();
     }
 };
