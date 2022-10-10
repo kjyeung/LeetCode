1f4c8831d7f69970407e029ee761261b646ac2b1
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-10 16:14:50 +0800

    Time: 8 ms (93.47%), Space: 9.7 MB (80.72%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
-    vector<vector<int>> ds{{-1, 0}, {0, -1}};
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        int table[m][n]; memset(table, INT_MAX, sizeof(table));
-        table[0][0] = grid[0][0];
-        
-        for(int i = 1; i < m; i++){
-            table[i][0]= table[i - 1][0] + grid[i][0];
+        for(int i = 1; i < m;i++){
+            grid[i][0] = grid[i][0] + grid[i - 1][0];
         }
-        for(int j = 1; j < n; j++){
-            table[0][j] = table[0][j - 1] + grid[0][j];
+        for(int j = 1; j < n;j++){
+            grid[0][j] = grid[0][j] + grid[0][j - 1];
         }
         
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                                
-                table[i][j] = min(table[i-1][j], table[i][j - 1]) + grid[i][j];
+        for(int i = 1; i < m;i++){
+            for(int j = 1; j < n;j++){
+                grid[i][j] = min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
             }
         }
-        return table[m - 1][n - 1];
+        return grid[m - 1][n - 1];
+        
     }
 };
