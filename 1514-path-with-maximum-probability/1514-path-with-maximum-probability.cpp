97e193f2df8a56dcac10fcc919e262a4626e5d55
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-12 16:32:28 +0800

    Time: 386 ms (49.31%), Space: 64.8 MB (90.85%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
+    struct comp{
+        bool operator()(pair<int, double>& a, pair<int, double>& b){
+            return a.second < b.second;
+        }
+    };
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<tuple<double, int>>> graph(n);
+        vector<pair<int ,double>> graph[n];
         for(int i = 0; i < edges.size(); i++){
             int a = edges[i][0];
             int b = edges[i][1];
             double p = succProb[i];
-            graph[a].push_back(make_tuple(p, b));
-            graph[b].push_back(make_tuple(p, a));
+            graph[a].push_back({b, p});
+            graph[b].push_back({a, p});
         }
         
         vector<double> ps(n, 0);
-        ps[start] = 1.0;
-        priority_queue<tuple<double, int>> pq;
-        pq.push(make_tuple(1.0, start));
-        
+        ps[start] = 1;
+        priority_queue<pair<int, double>, vector<pair<int, double>>, comp> pq;
+        pq.push({start, 1.0});
         while(!pq.empty()){
-            tuple<double, int> cur = pq.top();
+            auto cur = pq.top();
             pq.pop();
-            double p = get<0>(cur);
-            int to = get<1>(cur);
+            int to = cur.first;
+            double p = cur.second;
             if(p < ps[to]) continue;
             if(to == end) return p;
-            for(tuple<double, int> t : graph[to]){
-                double np = get<0>(t) * p;
-                int next = get<1>(t);
-                if(np > ps[next]){
-                    ps[next] = np;
-                    pq.push(make_tuple(np,next));
+            for(auto & t : graph[to]){
+                int next = t.first;
+                double newp = ps[to] * t.second;
+                if(newp > ps[next]){
+                    ps[next] = newp;
+                    pq.push({next, newp});
                 }
             }
+            
         }
         return 0;
     }
