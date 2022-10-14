33e69c91166b7de7b1c66cf56d196364438cca95
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-14 12:53:21 +0800

    Time: 21 ms (38.51%), Space: 9.7 MB (80.51%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int table[m][n]; memset(table, 50000, sizeof(table));
+        table[0][0] = grid[0][0];
+        for(int j = 1; j < n; j++){
+            table[0][j] = table[0][j - 1] + grid[0][j];
+        }
+        for(int i = 1; i <m ; i++){
+            table[i][0] = table[i - 1][0] +grid[i][0];
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+               table[i][j] = min(table[i - 1][j], table[i][j -1]) + grid[i][j]; 
+            }
+        }
+        return table[m - 1][n - 1];
+    }
+};
