aba911929c7ba5ef425737f89e267f8d44f4cd44
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-19 19:01:47 +0800

    Time: 5 ms (86.84%), Space: 7.2 MB (66.76%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,37 +2,36 @@ class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> res;
-        vector<string> board(n,string(n, '.'));
-        backtrack(res, board, 0, n);
+        vector<string> bd(n, string(n, '.'));
+        bt(res, bd, 0);
         return res;
     }
     
-    void backtrack(vector<vector<string>> & res, vector<string>& board, int start, int end){
-        if(start == end){
-            res.push_back(board);
+    void bt(vector<vector<string>> & res, vector<string>& bd, int row){
+        if(row == bd.size()){
+            res.push_back(bd);
             return;
         }
-        for(int i = 0; i < board.size();i++){
-            if(check(board, start, i)){
-                board[start][i] = 'Q';
-                backtrack(res, board, start + 1, end);
-                board[start][i] = '.';
-            }
+        for(int i = 0; i < bd.size(); i++){
+            if(!valid(bd, row, i)) continue;
+            bd[row][i] = 'Q';
+            bt(res, bd, row + 1);
+            bd[row][i] = '.';
         }
         return;
     }
     
-    bool check(vector<string> & board, int row, int col){
+    bool valid(vector<string>& bd, int row, int col){
         for(int i = 0; i < row;i++){
-            if(board[i][col] == 'Q') return false;
+            if(bd[i][col] == 'Q') return false;
         }
         
-        for(int i = row - 1, j = col - 1; i >= 0 && j >=0; i--,j--){
-            if(board[i][j] == 'Q') return false;
+        for(int i = row - 1, j = col - 1; i >= 0 && j>=0;i--,j--){
+            if(bd[i][j] == 'Q') return false;
         }
         
-        for(int i = row - 1, j = col + 1; i >=0 && j < board.size(); i--,j++){
-            if(board[i][j] == 'Q') return false;
+        for(int i = row - 1, j = col + 1; i >=0 && j < bd.size(); i--, j++){
+            if(bd[i][j] == 'Q') return false;
         }
         return true;
     }
