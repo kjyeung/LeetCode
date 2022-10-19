5ad0b69933ef28374aee04096d6e17b6f2f9d6da
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-10-19 19:26:46 +0800

    Time: 368 ms (14.54%), Space: 70.8 MB (53.19%) - LeetHub

---
+++ 0710-random-pick-with-blacklist/0710-random-pick-with-blacklist.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    unordered_map<int,int> table;
+    int size;
+    mt19937 gen;
+    uniform_int_distribution<> dis;
+public:
+    Solution(int n, vector<int>& blacklist) {
+        size = n - blacklist.size();
+        for(int b : blacklist){
+            table[b] = 777;
+        }
+        int last = n - 1;
+        for(int b : blacklist){
+            if(b >= size) continue;
+            while(table.count(last)) --last;
+            table[b] = last--;
+        }
+        
+        random_device rd;
+        gen = mt19937(rd());
+        dis = uniform_int_distribution<>(0, size - 1);
+    }
+    
+    int pick() {
+        int num = dis(gen);
+        if(table.count(num)) return table[num];
+        return num;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(n, blacklist);
+ * int param_1 = obj->pick();
+ */
