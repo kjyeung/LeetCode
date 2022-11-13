8ee87367fc3a6fafe99d90e475f8c246530a7e6e
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-13 13:58:44 +0800

    Time: 22 ms (75.00%), Space: 12.7 MB (50.00%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     int subarrayLCM(vector<int>& nums, int k) {
-        int left = 0, right = 0;
         int res = 0;
-        int n = nums.size();
-        int tmp = nums[left];
-        while(right < n){
-            int cur = nums[right];
-            if(cur == k) ++res;
-            ++right;
-            if(cur > k || k % cur || right == n){
-                while(left < right){
-                    int prev = nums[left];
-                    for(int i = left + 1; i < right; i++) {
-                        prev = lcm(prev, nums[i]);
-                        if(prev == k) res += 1;
-                    }
-                    ++left;
-                }
-            }else{
+        unordered_map<int,int> m;
+        for(int i = 0, j = 0; i < nums.size(); ++i){
+            ++m[nums[i]];
+            unordered_map<int,int> m1;
+            for(auto [d, cnt] : m){
+                int lcm = d * nums[i] / gcd(d,nums[i]);
+                if(lcm == k)
+                    res += cnt;
+                if(k % lcm == 0)
+                    m1[lcm] += cnt;
                 
             }
-            
-             
+            swap(m , m1);
             
         }
-        
         return res;
         
     }
-    
-    int gcd(int a, int b){
-        while(b^=a^=b^=a%=b);
-        return a;
-    }
-    int lcm (int a, int b){
-        return a/gcd(a, b)*b;
-    }
-    
-
 };
