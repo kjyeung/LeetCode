d5d6e3ace5bee22ae9fd85bd7e86899747ed0735
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-14 06:59:55 +0800

    Time: 1907 ms (44.74%), Space: 558.6 MB (37.86%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -2,50 +2,53 @@ class Trie{
 public:
     bool isEnd;
     Trie* next[26];
-    
     Trie(){
         isEnd = false;
         memset(next, 0, sizeof(next));
     }
-    
-    void insert(string word){
+    void insert(const string & word){
         Trie* node = this;
         for(char c : word){
             c -= 'a';
-            if(node->next[c]== nullptr) node->next[c] = new Trie();
+            if(node->next[c] == nullptr) node->next[c] = new Trie();
             node = node->next[c];
         }
         node->isEnd = true;
+        return;
     }
-    
-    bool search(string word, int idx){
+    bool search(const string& word, int idx){
+        if(idx == word.size()) return this->isEnd;
         Trie* node = this;
-        if(idx == word.size()) return node->isEnd;
-        if(word[idx] != '.'){
-            Trie* child = node->next[word[idx] - 'a'];
-            if(child) return child->search(word, idx + 1);
+        char c = word[idx];
+        if(c != '.'){
+            Trie* child = node->next[c-'a'];
+            return child && child->search(word, idx + 1);
         }else{
-            for(int i = 0; i < 26 ;i++){
+            for(int i = 0; i < 26 ; i++){
                 Trie* child = node->next[i];
-                if(child && child->search(word, idx + 1)) return true;
+                if(child && child->search(word,idx + 1))  return true;
             }
         }
         return false;
     }
+    
 };
 class WordDictionary {
 public:
-    Trie* root = nullptr;
+    Trie* root;
     WordDictionary() {
         root = new Trie();
+        
     }
     
     void addWord(string word) {
         root->insert(word);
+        
     }
     
     bool search(string word) {
-        return root->search(word, 0 );
+        return root->search(word, 0);
+        
     }
 };
 
