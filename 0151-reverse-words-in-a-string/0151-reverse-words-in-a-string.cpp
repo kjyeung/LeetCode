87cfc208216553d15dd227aa52ba25ef4399dca0
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-15 18:58:30 +0800

    Time: 0 ms (100.00%), Space: 7.1 MB (82.94%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,28 +4,28 @@ public:
         removeSpace(s);
         reverse(s, 0, s.size() - 1);
         int start = 0;
-        while (start < s.size()){
-            int left = start, right = start;
+        while(start < s.size()){
+            int left = start;
+            int right = left;
             while(right < s.size() && s[right] != ' ') ++right;
             start = right + 1;
             reverse(s, left, right - 1);
         }
-
         return s;
+        
     }
-    
-    void removeSpace(string& s){
+    void removeSpace(string & s){
         int slow = 0;
         for(int i = 0; i < s.size(); i++){
-            if(s[i] == ' ' && (slow == 0 || (slow > 0 && s[slow - 1] == ' ')))  continue;
-            s[slow++] = s[i];
+            if(s[i] == ' ' && (slow == 0 || (slow > 0 && s[slow - 1] == ' '))) continue;
+                s[slow++] = s[i];
         }
         s = s.substr(0, slow);
         if(s.back() == ' ') s.pop_back();
-        return;
     }
-    void reverse(string & s, int start, int end){
-        while(start < end) swap(s[start++], s[end--]);
-        return;
+    
+    void reverse(string & s, int left, int right){
+        if(left >= right) return;
+        while(left < right) swap(s[left++], s[right--]);
     }
 };
