72600f3b76a92f122d80b2f6e430b4e80d0b8d93
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-21 03:50:34 +0800

    Time: 175 ms (25.89%), Space: 42.2 MB (5.11%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int countTriples(int n) {
-        if(n == 1) return 0;
+        unordered_map<int,int> table;
+        for(int i = 1; i <= n;i++){
+            ++table[i*i];
+        }
         int res = 0;
-        int max = n * n;
-        for(int i = 1; i <= n; i++){
-            for(int j = i + 1; j <= n; j++){
-                for(int k = j + 1; k <= n; k++){
-                    if(pow(i, 2) + pow(j, 2) == pow(k, 2)) res+=2;
-                }
+        for(int i = 1; i <n; i++){
+            for(int j = i + 1; i * i + j * j <= n * n; j++){
+                res += table[i * i + j * j] * 2;
+                
             }
         }
         return res;
-        
     }
+        
 };
