ee754ce5f28e9b774240ea43a8f35089c1913aae
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-23 18:43:57 +0800

    Time: 423 ms (49.05%), Space: 66.2 MB (68.06%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    struct comp{
-        bool operator()(pair<int, double>& a, pair<int, double>& b){
-            return a.second < b.second;
-        }
-    };
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<pair<int ,double>> graph[n];
+        priority_queue<tuple<double, int>, vector<tuple<double, int>>, less<tuple<double, int>>> pq;
+        
+        vector<vector<tuple<double, int>>> graph(n);
         for(int i = 0; i < edges.size(); i++){
             int a = edges[i][0];
             int b = edges[i][1];
             double p = succProb[i];
-            graph[a].push_back({b, p});
-            graph[b].push_back({a, p});
+            graph[a].push_back({p, b});
+            graph[b].push_back({p, a});
         }
+        vector<double> dis(n, 0);
+        dis[start] = 1.0;
+        pq.push({1.0, start});
         
-        vector<double> ps(n, 0);
-        ps[start] = 1;
-        priority_queue<pair<int, double>, vector<pair<int, double>>, comp> pq;
-        pq.push({start, 1.0});
         while(!pq.empty()){
             auto cur = pq.top();
             pq.pop();
-            int to = cur.first;
-            double p = cur.second;
-            if(p < ps[to]) continue;
+            double p = get<0>(cur);
+            int to = get<1>(cur);
             if(to == end) return p;
-            for(auto & t : graph[to]){
-                int next = t.first;
-                double newp = ps[to] * t.second;
-                if(newp > ps[next]){
-                    ps[next] = newp;
-                    pq.push({next, newp});
+            if(p < dis[to]) continue;
+            for(auto e : graph[to]){
+                int next = get<1>(e);
+                double newp = p * get<0>(e);
+                if(newp > dis[next]){
+                    dis[next] = newp;
+                    pq.push({newp, next});
                 }
             }
-            
         }
-        return 0;
+        return dis[end]; 
+        
+        
     }
 };
