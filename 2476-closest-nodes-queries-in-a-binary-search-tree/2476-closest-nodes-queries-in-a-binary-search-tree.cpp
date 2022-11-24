6a2d8ed0b6bc65aa85c9e66bf3df1186f9009ebe
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-24 17:16:11 +0800

    Time: 608 ms (64.67%), Space: 157.1 MB (8.37%) - LeetHub

--- 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -1,25 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> res;
-    vector<int> flat;
     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
-        traverse(root);
-        
-        for(int & q : queries){
-            auto it = lower_bound(flat.begin(), flat.end(), q);
-            if (it != flat.end() && *it == q) 
-                res.push_back({q, q});
-            else
-                res.push_back({it == flat.begin()? -1 : *prev(it), it == flat.end()? -1 : *it});
+        vector<int> nums;
+        flatten(root, nums);
+        vector<vector<int>> res;
+        for(int q : queries){
+            auto left = lower_bound(nums.begin(), nums.end(), q);
+            if(left != nums.end() && *left == q) res.push_back({q, q});
+            else if(left == nums.end()) res.push_back({nums.back(), -1});
+            else if(left == nums.begin()) res.push_back({-1, nums.front()});
+            else res.push_back({*prev(left), *left});
         }
         return res;
         
     }
-    void traverse(TreeNode* root){
+    void flatten(TreeNode* root, vector<int>& nums){
         if(root == nullptr) return;
-        traverse(root->left);
-        flat.push_back(root->val);
-        traverse(root->right);
+        flatten(root->left, nums);
+        nums.push_back(root->val);
+        flatten(root->right, nums);
         return;
     }
 };
