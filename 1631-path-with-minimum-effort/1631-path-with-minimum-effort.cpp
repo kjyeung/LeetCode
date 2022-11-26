f62c46d81188c5bffad5d4194be81149031f8243
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-26 14:18:05 +0800

    Time: 184 ms (89.25%), Space: 20 MB (65.87%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
-
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m = heights.size();
-        int n = heights[0].size();
-        //vector<vector<int>> table(m, vector<int>(n, INT_MAX));
-        int table[m][n]; memset(table, 1000001, sizeof(table));
-        table[0][0] = 0;
-        
+        int n = heights.front().size();
+        vector<vector<int>> costs(m, vector<int>(n, INT_MAX));
+        costs[0][0] = 0;
+        vector<vector<int>> ds{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
         priority_queue<tuple<int, int, int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> pq;
         pq.push({0, 0, 0});
-        vector<vector<int>> ds{{0, 1}, {1, 0},{0, -1}, {-1, 0}};
-        
         while(!pq.empty()){
-            auto cur = pq.top();
+            tuple<int,int,int>  cur = pq.top();
             pq.pop();
+            int cost = get<0>(cur);
             int i = get<1>(cur);
             int j = get<2>(cur);
-            int cost = get<0>(cur);
-            
-            if(cost > table[i][j]) continue;
-            
             if(i == m - 1 && j == n - 1) return cost;
-            
-            for(auto &d : ds){
+            if(cost > costs[i][j]) continue;
+            for(vector<int> & d : ds){
                 int ii = i + d[0];
                 int jj = j + d[1];
-                if(ii < 0 || ii > m-1 || jj < 0 || jj > n - 1) continue;
-                int newc = max(cost, abs(heights[i][j] - heights[ii][jj]));
-                if(newc < table[ii][jj]) {
-                    table[ii][jj] = newc;
-                    pq.push({newc, ii, jj});
+                if(ii < 0 || ii > m - 1 || jj < 0 || jj > n - 1) continue;
+                int newcost = max(cost, abs(heights[ii][jj] - heights[i][j]));
+                if(newcost < costs[ii][jj]){
+                    costs[ii][jj] = newcost;
+                    pq.push({newcost, ii ,jj });
                 }
-                
             }
-            
         }
-        return table[m - 1][n - 1];
+        return costs[m - 1][n - 1];
+        
     }
 };
