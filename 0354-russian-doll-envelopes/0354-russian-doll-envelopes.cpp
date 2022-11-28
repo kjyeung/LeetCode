5577596fa65fdf176717cd70db3ed09a68ca083e
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-28 20:09:37 +0800

    Time: 354 ms (99.15%), Space: 77.5 MB (68.45%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,31 +1,32 @@
+struct comp{
+    bool operator()(vector<int>& a, vector<int>&b){
+        if(a[0] < b[0]) return true;
+        if(a[0] == b[0]) return a[1] >= b[1];
+        return false;
+    }
+};
 class Solution {
 public:
-    struct comp{
-        bool operator()(vector<int>& a, vector<int>& b){
-            if(a[0]==b[0]) return a[1] >= b[1];
-            return a[0] < b[0];
-        }
-    };
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(), envelopes.end(), comp());
         vector<int> tmp;
-        for(vector<int>& env : envelopes){
+        for(vector<int>& e : envelopes){
             if(tmp.empty()){
-                tmp.push_back(env[1]);
+                tmp.push_back(e[1]);
                 continue;
             }
             int left = 0, right = tmp.size();
             while(left < right){
                 int mid = left + (right - left) / 2;
-                if(tmp[mid] > env[1]){
+                if(tmp[mid]>e[1]){
                     right = mid;
-                }else if(tmp[mid] < env[1]){
-                    left = mid + 1;
-                }else right = mid;
+                }else if(tmp[mid] == e[1])right = mid;
+                else left = mid + 1;
             }
-            if(left == tmp.size()) tmp.push_back(env[1]);
-            else tmp[right] = env[1];
+            if(left == tmp.size()) tmp.push_back(e[1]);
+            tmp[left] = e[1];
         }
         return tmp.size();
+        
     }
 };
