9da66581a5412b02f02b10d828a30856b01ff86f
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-11-29 11:13:10 +0800

    Time: 932 ms (24.55%), Space: 155.6 MB (18.88%) - LeetHub

--- 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -15,17 +15,17 @@ public:
     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
         flatten(root);
         vector<vector<int>> res;
-        for(int & q : queries){
+        for(int q : queries){
             auto it = lower_bound(nums.begin(), nums.end(), q);
-            if(it != nums.end() && *it == q){
+            if(it != nums.end() && *it == q)
                 res.push_back({q, q});
-            }else{
-                res.push_back({it == nums.begin()? -1 : *prev(it), it == nums.end()? -1 : *it});
-            }
+            else
+                res.push_back({it == nums.begin()? -1 : *prev(it), it == nums.end()? -1: *it});
         }
         return res;
         
     }
+    
     void flatten(TreeNode* root){
         if(root == nullptr) return;
         flatten(root->left);
