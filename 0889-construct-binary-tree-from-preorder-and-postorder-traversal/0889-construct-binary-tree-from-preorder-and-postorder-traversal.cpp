2d018b183798ebbe65bf8b9b89aea4ab8253ff50
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-12-01 10:43:08 +0800

    Time: 29 ms (33.11%), Space: 25.4 MB (72.85%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -11,27 +11,16 @@
  */
 class Solution {
 public:
-    unordered_map<int,int> postidx;
+    int preidx = 0, postidx = 0;
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-        int n = preorder.size();
-        for(int i = 0; i < n; i++){
-            postidx[postorder[i]] = i;
+        TreeNode* root = new TreeNode(preorder[preidx++]);
+        if(root->val != postorder[postidx]){
+            root->left = constructFromPrePost(preorder, postorder);
         }
-        return cfpp(preorder, 0, n - 1, postorder, 0, n - 1);
-    }
-    
-    TreeNode* cfpp(vector<int>& preorder, int preleft, int preright, vector<int>& postorder, int postleft, int postright){
-        if(preleft > preright || postleft > postright) return nullptr;
-        if(preleft == preright || postleft == postright){
-            return new TreeNode(preorder[preleft]);
+        if(root->val != postorder[postidx]){
+            root->right = constructFromPrePost(preorder, postorder);
         }
-        int rootval = preorder[preleft];
-        TreeNode* root = new TreeNode(rootval);
-        int subrootval = preorder[preleft + 1];
-        int subrootpostidx = postidx[subrootval];
-        int leftsize = subrootpostidx - postleft ;
-        root->left = cfpp(preorder, preleft + 1, preleft + leftsize + 1, postorder, postleft, subrootpostidx);
-        root->right = cfpp(preorder, preleft + leftsize + 2, preright, postorder,subrootpostidx + 1, postright - 1);
+        ++postidx;
         return root;
     }
 };
