bb8523f91181ece4bdf6e12a093dddbc7f3b3514
kjyeung <58033474+kjyeung@users.noreply.github.com>
2022-12-04 19:38:08 +0800

    Time: 6 ms (78.53%), Space: 8.7 MB (61.25%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
+        vector<bool> in(201, false);
         int res = 0;
-        vector<bool> table(201, false);
-        for(int i = 0; i < nums.size(); i++){
-            table[nums[i]] = true;
-            if(nums[i] < 2 *diff) continue;
-            if(table[nums[i] - diff] && table[nums[i]-2*diff]) ++res;
+        for(int i = 0; i < nums.size();i++){
+            in[nums[i]] = true;
+            if(nums[i] >= 2*diff && in[nums[i] - diff] && in[nums[i] - 2*diff]) ++res;
         }
         return res;
-        
         
     }
 };
