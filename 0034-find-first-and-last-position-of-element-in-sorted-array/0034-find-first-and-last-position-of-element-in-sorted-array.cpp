fe4a1935fc056804c649bac6593def6347300628
kjyeung <58033474+kjyeung@users.noreply.github.com>
2023-01-29 15:59:32 +0800

    Time: 12 ms (51.51%), Space: 13.7 MB (69.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        if(nums.empty()) return {-1,-1};
         vector<int> res;
-        int left = 0, right = nums.size() - 1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] >= target){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
+        int left = 0, right = nums.size();
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(nums[mid]==target) right = mid;
+            else if(nums[mid] > target) right = mid;
+            else left = mid + 1;
         }
-        if(left == nums.size() || nums[left] != target) return {-1, -1};
-        
+        if(left == nums.size() || nums[left] != target) left = -1;
         res.push_back(left);
-        left = 0, right = nums.size() - 1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] <= target){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
-            }
+        
+        left = 0, right = nums.size();
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(nums[mid]==target) left = mid + 1;
+            else if(nums[mid] > target) right = mid;
+            else left = mid + 1;
         }
-        if(right < 0 || nums[right] != target) return {-1, -1};
-        res.push_back(right);
+        if(left-1 < 0 || nums[left - 1] != target) left = -1;
+        else left -= 1;
+        res.push_back(left);
         return res;
-        
-        
-        
+
     }
 };
