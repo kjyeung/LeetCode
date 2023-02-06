f741fcc1381b09f3db746e6b4648f338edf20a38
kjyeung <58033474+kjyeung@users.noreply.github.com>
2023-02-06 10:18:28 +0800

    Time: 17 ms (55.83%), Space: 10.7 MB (64.03%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -3,20 +3,25 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> res;
         vector<int> path;
-        traverse(graph, path, res, 0);
+        path.push_back(0);
+        for(int next : graph[0]){
+            traverse(graph, res, path, next);
+        }
         return res;
     }
     
-    void traverse(vector<vector<int>>& graph, vector<int>& path, vector<vector<int>>& res, int start){
-        path.push_back(start);
-        if(start == graph.size() - 1){
+    void traverse(vector<vector<int>>& graph, vector<vector<int>> & res, vector<int>& path, int next){
+        path.push_back(next);
+        if(next == graph.size() - 1){
             res.push_back(path);
-        }else{
-            for(int t : graph[start]){
-                traverse(graph, path, res, t);
+        }
+        else{
+            for(int s : graph[next]){
+                traverse(graph, res, path, s);
             }
         }
         
         path.pop_back();
+        return ;
     }
 };
