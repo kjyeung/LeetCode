ad6b1af2c74ed928ae26a66327e865273484f8c9
kjyeung <58033474+kjyeung@users.noreply.github.com>
2023-02-08 15:26:16 +0800

    Time: 195 ms (88.26%), Space: 66.3 MB (56.68%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,38 +1,42 @@
+struct comp{
+    bool operator()(const pair<double,int>&a, const pair<double, int>&b){
+        return a.first < b.first;
+    }
+};
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        priority_queue<pair<double, int>> pq;
+        vector<double> probs(n, 0);
+        probs[start] = 1;
         
-        vector<vector<pair<double, int>>> graph(n);
-        for(int i = 0; i < edges.size(); i++){
+        
+        priority_queue<pair<double,int>, vector<pair<double,int>>,comp> pq; 
+        vector<vector<pair<double,int>>> graph(n);
+        for(int i = 0; i < edges.size();i++){
             int a = edges[i][0];
             int b = edges[i][1];
             double p = succProb[i];
             graph[a].push_back({p, b});
             graph[b].push_back({p, a});
         }
-        vector<double> dis(n, 0);
-        dis[start] = 1.0;
         pq.push({1.0, start});
         
         while(!pq.empty()){
-            auto cur = pq.top();
+            pair<double,int> cur = pq.top();
             pq.pop();
+            int node = cur.second;
             double p = cur.first;
-            int to = cur.second;
-            if(to == end) return p;
-            if(p < dis[to]) continue;
-            for(auto e : graph[to]){
-                int next = e.second;
-                double newp = p * e.first;
-                if(newp > dis[next]){
-                    dis[next] = newp;
-                    pq.push({newp, next});
+            if(p < probs[node]) continue;
+            if(node == end) return p;
+            for(pair<double,int>& np : graph[node]){
+                double newp = p * np.first;
+                int newnode = np.second;
+                if(newp > probs[newnode]){
+                    probs[newnode] = newp;
+                    pq.push({newp, newnode});
                 }
             }
         }
-        return dis[end]; 
-        
-        
+        return 0;
     }
 };
