48d123c8288698d2610b56b8407c4b677290a706
kjyeung <58033474+kjyeung@users.noreply.github.com>
2023-02-08 16:17:17 +0800

    Time: 7 ms (74.44%), Space: 7.3 MB (66.95%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> res;
-        vector<string> bd(n, string(n, '.'));
-        bt(res, bd, 0);
+        vector<string> board(n, string(n, '.'));
+        bt(res, board, 0);
         return res;
     }
     
-    void bt(vector<vector<string>> & res, vector<string>& bd, int row){
-        if(row == bd.size()){
-            res.push_back(bd);
+    void bt(vector<vector<string>>& res, vector<string>& board, int next){
+        if(next == board.size()){
+            res.push_back(board);
             return;
         }
-        for(int i = 0; i < bd.size(); i++){
-            if(!valid(bd, row, i)) continue;
-            bd[row][i] = 'Q';
-            bt(res, bd, row + 1);
-            bd[row][i] = '.';
+        
+        for(int i = 0; i < board[0].size();i++){
+            board[next][i] = 'Q';
+            if(valid(board, next, i)){
+                bt(res,board, next + 1);
+            }
+            board[next][i] = '.';
         }
         return;
     }
     
-    bool valid(vector<string>& bd, int row, int col){
+    bool valid(vector<string>& board, int row, int col){
         for(int i = 0; i < row;i++){
-            if(bd[i][col] == 'Q') return false;
+            if(board[i][col] == 'Q') return false;
         }
-        
-        for(int i = row - 1, j = col - 1; i >= 0 && j>=0;i--,j--){
-            if(bd[i][j] == 'Q') return false;
+        for(int i = row - 1,j = col - 1; i >=0 &&j >=0; i--,j--){
+            if(board[i][j] == 'Q') return false;
         }
-        
-        for(int i = row - 1, j = col + 1; i >=0 && j < bd.size(); i--, j++){
-            if(bd[i][j] == 'Q') return false;
+        for(int i = row - 1,j = col + 1; i >=0 && j < board[0].size();i--,j++){
+            if(board[i][j] == 'Q') return false;
         }
         return true;
     }
